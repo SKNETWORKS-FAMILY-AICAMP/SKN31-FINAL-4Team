d55665e619ddf98445f52a1fb53760664a50e9ce
Jeon Seoyeon <wjstjdus20@gmail.com>
2026-08-31 15:06:17 +0900

    [AWS맞춤 커밋] 초기 셋업

--- backend/apps/crawler/management/commands/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Crawler management command package."""
--- backend/apps/naver/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NaverConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.naver"
