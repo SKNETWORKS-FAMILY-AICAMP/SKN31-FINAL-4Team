7eab897dd186cccd952bce7bc67ac7ac38ec72b2
Jeon Seoyeon <wjstjdus20@gmail.com>
2026-09-11 15:47:16 +0900

    [진입 url 수정: feedit-dashboard- admin page]

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(
+            url="/admin-dashboard/",
+            permanent=False,
+        ),
+        name="root_redirect",
+    ),
+
     path("admin/", admin.site.urls),
     path("admin-dashboard/", include("apps.dashboard.urls")),
     path("api/", include("apps.api.urls")),
