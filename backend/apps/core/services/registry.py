3eb90a3c71fa9651f9b5413c5263d3e4581d0fd0
Ko Hyun-A <hellene0708@gmail.com>
2026-09-11 16:03:03 +0900

    ably수집

--- backend/apps/core/services/registry.py
+++ backend/apps/core/services/registry.py
@@ -2,6 +2,7 @@ from importlib import import_module
 
 
 PIPELINE_REGISTRY = {
+    "ABLY": "collection.ably.pipeline.AblyPipeline",
     "KREAM": "collection.kream.pipeline.KreamPipeline",
     "MUSINSA": "collection.musinsa.pipeline.MusinsaPipeline",
     "MUSINSA_USED": "collection.musinsa_used.pipeline.MusinsaUsedPipeline",
